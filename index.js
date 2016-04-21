baf7c3042077bfd5392e91e0e1f729b411e33569
Christian Alfoni <christianalfoni@gmail.com>
2016-04-21 17:13:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function ModulesProvider(context, execution, controller) {
+  var modules = controller.getModules()
+  var services = controller.getServices()
+  context.modules = Object.keys(modules).reduce(function (contextModules, key) {
+    var modulePath = key.split('.')
+    var module = modulePath.reduce(function (contextModules, pathKey) {
+      contextModules[pathKey] = contextModules[pathKey] || {}
+      return contextModules[pathKey]
+    }, contextModules)
+    module.meta = modules[key]
+    module.state = context.state.select(modulePath)
+    module.services = modulePath.reduce(function (services, key) {
+      return services[key] || {}
+    }, services)
+
+    if (
+      execution.options.modulePath &&
+      execution.options.modulePath.join('.') === key) {
+        context.module = module
+      }
+
+    return contextModules
+  }, {})
+
+  return context
+}
+
+module.exports = ModulesProvider
