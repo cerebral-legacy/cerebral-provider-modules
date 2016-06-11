e35bb9439b08daee022de6d561c69f80e74fb49d
Austin Wei <awei01@gmail.com>
2016-06-11 16:19:48 -0700

    properly resolving aliased module's services

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ function ModulesProvider(context, execution, controller) {
   var modules = controller.getModules()
   var services = controller.getServices()
   context.modules = Object.keys(modules).reduce(function (contextModules, key) {
-    var modulePath = key.split('.')
+    var modulePath = key.split('.');
+    var absModulePath = modules[key].path;
     var module = modulePath.reduce(function (contextModules, pathKey) {
       contextModules[pathKey] = contextModules[pathKey] || {}
       return contextModules[pathKey]
     }, contextModules)
     module.meta = modules[key].meta
-    module.state = context.state.select(modulePath)
-    module.services = modulePath.reduce(function (services, key) {
+    module.path = absModulePath;
+    module.state = context.state.select(absModulePath)
+    module.services = absModulePath.reduce(function (services, key) {
       return services[key] || {}
     }, services)
 
